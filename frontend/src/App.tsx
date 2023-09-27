f02ff410b06a6537a6a31d2ccf1db5274adf898a
test-keiju <63958587+keiju-web@users.noreply.github.com>
2023-09-28 01:41:42 +0900

    turn off Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ROUTES } from 'constants/route'
 
 import { CssBaseline } from '@mui/material'
-import Auth from 'components/auth/Auth'
+// import Auth from 'components/auth/Auth'
 import Layout from 'components/layout/Layout'
 import ScrollTop from 'components/util/ScrollToTop'
 import { useAuth } from 'hooks/use-auth'
@@ -12,29 +12,29 @@ import { useAuth } from 'hooks/use-auth'
 const App: FC = () => {
   const { session, isAuthorizing } = useAuth()
 
-  if (isAuthorizing) {
-    return <></>
-  }
+  // if (isAuthorizing) {
+  //   return <></>
+  // }
 
   return (
     <div className='App'>
       {/* Init CSS */}
       <CssBaseline />
 
-      {session ? (
-        <BrowserRouter>
-          <ScrollTop />
-          <Layout>
-            <Routes>
-              {ROUTES.map((route, key) => {
-                return <Route key={key} {...route} />
-              })}
-            </Routes>
-          </Layout>
-        </BrowserRouter>
-      ) : (
+      {/* {session ? ( */}
+      <BrowserRouter>
+        <ScrollTop />
+        <Layout>
+          <Routes>
+            {ROUTES.map((route, key) => {
+              return <Route key={key} {...route} />
+            })}
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+      {/* ) : (
         <Auth />
-      )}
+      )} */}
     </div>
   )
 }
